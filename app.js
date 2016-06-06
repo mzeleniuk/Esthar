c283097ee796a8aeb6a850f8e400467322cb8510
Michael Zelenyuk <zell.xp@gmail.com>
2016-06-06 17:34:19 +0300

    Add simple user form

--- app.js
+++ app.js
@@ -9,5 +9,16 @@ angular.module('Esthar', []).controller('MainCtrl', [
       {title: 'post 4', upvotes: 9},
       {title: 'post 5', upvotes: 4}
     ];
+
+    $scope.addPost = function() {
+      if(!$scope.title || $scope.title === '') { return; }
+
+      $scope.posts.push({title: $scope.title, upvotes: 0});
+      $scope.title = '';
+    };
+
+    $scope.incrementUpvotes = function(post) {
+      post.upvotes += 1;
+    }
   }
 ]);
