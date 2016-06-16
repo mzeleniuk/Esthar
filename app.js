a31e0d4023c1b3a2bc90c15a59ebb53b727e0034
Michael Zelenyuk <zell.xp@gmail.com>
2016-06-16 12:16:27 +0300

    Add Posts page and comments

--- app.js
+++ app.js
@@ -10,6 +10,12 @@ Esthar.config([
         url: '/home',
         templateUrl: '/home.html',
         controller: 'MainCtrl'
+      })
+
+      .state('posts', {
+        url: '/posts/{id}',
+        templateUrl: '/posts.html',
+        controller: 'PostsCtrl'
       });
 
     $urlRouterProvider.otherwise('home');
@@ -19,11 +25,11 @@ Esthar.config([
 Esthar.factory('posts', [function() {
   var object = {
     posts: [
-      {title: 'Post 1', upvotes: 5},
-      {title: 'Post 2', upvotes: 2},
-      {title: 'Post 3', upvotes: 15},
-      {title: 'Post 4', upvotes: 9},
-      {title: 'Post 5', upvotes: 4}
+      {title: 'Post 1', upvotes: 5, comments: [{author: 'Angy', body: 'Great!', upvotes: 0}]},
+      {title: 'Post 2', upvotes: 2, comments: [{author: 'Jane', body: 'Hi!', upvotes: 0}]},
+      {title: 'Post 3', upvotes: 15, comments: [{author: 'Joe', body: 'Cool post!', upvotes: 0}]},
+      {title: 'Post 4', upvotes: 9, comments: [{author: 'Michael', body: 'This is test.', upvotes: 0}]},
+      {title: 'Post 5', upvotes: 4, comments: [{author: 'Hinata', body: ':)', upvotes: 0}]}
     ]
   };
 
@@ -43,7 +49,11 @@ Esthar.controller('MainCtrl', [
       $scope.posts.push({
         title: $scope.title,
         link: $scope.link,
-        upvotes: 0
+        upvotes: 0,
+        comments: [
+          {author: 'Joe', body: 'Cool post!', upvotes: 0},
+          {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+        ]
       });
 
       $scope.title = '';
@@ -55,6 +65,35 @@ Esthar.controller('MainCtrl', [
 
     $scope.incrementUpvotes = function(post) {
       post.upvotes += 1;
+    };
+  }
+]);
+
+Esthar.controller('PostsCtrl', [
+  '$scope',
+  '$stateParams',
+  'posts',
+
+  function($scope, $stateParams, posts) {
+    $scope.post = posts.posts[$stateParams.id];
+
+    $scope.addComment = function() {
+      if (!$scope.body || $scope.body === '') { return; }
+
+      $scope.post.comments.push({
+        body: $scope.body,
+        author: 'user',
+        upvotes: 0
+      });
+
+      $scope.body = '';
+
+      $scope.form.$setPristine();
+      $scope.form.$setUntouched();
+    };
+
+    $scope.incrementUpvotes = function(comment) {
+      comment.upvotes += 1;
     };
   }
 ]);
