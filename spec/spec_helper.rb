4beef045cefda54cb0432ba7da256a988e80a95e
Michael Zelenyuk <zell.xp@gmail.com>
2016-10-06 10:39:08 +0300

    Configure CircleCI to use SimpleCov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,6 +18,13 @@
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 
 require 'simplecov'
+
+# Save to CircleCI's artifacts directory if we're on CircleCI
+if ENV['CIRCLE_ARTIFACTS']
+  dir = File.join(ENV['CIRCLE_ARTIFACTS'], 'coverage')
+  SimpleCov.coverage_dir(dir)
+end
+
 SimpleCov.start
 
 RSpec.configure do |config|
