42a0893b2f44aa650e276dd5bafdb9bd98767f88
Michael Zelenyuk <zell.xp@gmail.com>
2016-10-14 17:27:10 +0300

    Add Unit tests for Comment and Post models

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # Associations
+  it { is_expected.to belong_to(:post) }
+
+  # Validations
+  it { is_expected.to validate_presence_of(:body) }
+  it { is_expected.to validate_presence_of(:post) }
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # Associations
+  it { is_expected.to have_many(:comments) }
+
+  # Validations
+  it { is_expected.to validate_presence_of(:title) }
 end
