6c048bcd495e32b905673ef5acc31ed346f01cd4
Michael Zelenyuk <zell.xp@gmail.com>
2016-11-03 17:56:51 +0200

    Query Rails Back-end for all posts using the index route

--- app/assets/javascripts/posts/posts.js
+++ app/assets/javascripts/posts/posts.js
@@ -1,12 +1,12 @@
-Esthar.factory('posts', [function () {
+Esthar.factory('posts', ['$http', function ($http) {
   var object = {
-    posts: [
-      {title: 'Post 1', upvotes: 5, comments: [{author: 'Angy', body: 'Great!', upvotes: 0}]},
-      {title: 'Post 2', upvotes: 2, comments: [{author: 'Jane', body: 'Hi!', upvotes: 0}]},
-      {title: 'Post 3', upvotes: 15, comments: [{author: 'Joe', body: 'Cool post!', upvotes: 0}]},
-      {title: 'Post 4', upvotes: 9, comments: [{author: 'Michael', body: 'This is test.', upvotes: 0}]},
-      {title: 'Post 5', upvotes: 4, comments: [{author: 'Drake', body: ':)', upvotes: 0}]}
-    ]
+    posts: []
+  };
+
+  object.getAll = function () {
+    return $http.get('/posts.json').success(function (data) {
+      angular.copy(data, object.posts);
+    });
   };
 
   return object;
