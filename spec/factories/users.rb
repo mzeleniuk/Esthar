68ba7db1e5e0837af549a3cc482ec317517d21ea
Michael Zelenyuk <zell.xp@gmail.com>
2016-11-08 13:45:54 +0200

    Add factory and model tests for User

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,13 @@
 FactoryGirl.define do
   factory :user do
-    
+    first_name 'James'
+    last_name 'Sanders'
+    sequence(:username) { |n| "user_#{n}" }
+    sequence(:email) { |n| "user_#{n}@mail.com" }
+    password 'secret'
+    password_confirmation 'secret'
+    date_of_birth Date.new(1990, 4, 20)
+    phone 1123456789
+    website 'https://google.com'
   end
 end
