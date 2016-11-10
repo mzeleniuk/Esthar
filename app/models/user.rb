a1aa554be964e3637578db80ee5123018d21d387
Michael Zelenyuk <zell.xp@gmail.com>
2016-11-10 16:29:41 +0200

    Add Angular validations to Registration form

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,9 +11,9 @@ class User < ActiveRecord::Base
 
   validates :email, length: {maximum: 50},
             format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
-  validates :username, length: {maximum: 15}
+  validates :username, length: {maximum: 15}, uniqueness: {case_sensitive: false}
   validates :password, length: {minimum: 6}
-  validates :first_name, :last_name, length: {maximum: 25}
-  validates :phone, numericality: {only_integer: true}, length: {maximum: 20}
-  validates :website, length: {maximum: 100}
+  validates :first_name, :last_name, length: {maximum: 25}, allow_blank: true
+  validates :phone, numericality: {only_integer: true}, length: {maximum: 20}, allow_blank: true
+  validates :website, length: {maximum: 100}, allow_blank: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  let!(:user) { create :user }
+
   # Validations
   it { is_expected.to validate_presence_of(:email) }
   it { is_expected.to validate_length_of(:email).is_at_most(50).with_message('is too long (maximum is 50 characters)') }
+  it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
   it { is_expected.to allow_value('user@example.com').for(:email) }
   it { is_expected.to allow_value('user@example.COM').for(:email) }
   it { is_expected.to allow_value('A_US-ER@f.b.org').for(:email) }
@@ -15,6 +18,7 @@ RSpec.describe User, type: :model do
   it { is_expected.to_not allow_value('foo@bar+baz.com').for(:email) }
   it { is_expected.to validate_presence_of(:username) }
   it { is_expected.to validate_length_of(:username).is_at_most(15).with_message('is too long (maximum is 15 characters)') }
+  it { is_expected.to validate_uniqueness_of(:username).case_insensitive }
   it { is_expected.to validate_length_of(:password).is_at_least(6).with_message('is too short (minimum is 6 characters)') }
   it { is_expected.to validate_length_of(:first_name).is_at_most(25).with_message('is too long (maximum is 25 characters)') }
   it { is_expected.to validate_length_of(:last_name).is_at_most(25).with_message('is too long (maximum is 25 characters)') }
