5191e856fb196b6ecd6fc48aa95c6cff69d80159
Michael Zelenyuk <zell.xp@gmail.com>
2016-11-11 14:38:18 +0200

    Add controllers authentication Unit tests

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -1,34 +1,71 @@
 require 'rails_helper'
 
 RSpec.describe CommentsController, type: :controller do
+  let(:user) { create :user }
   let(:new_post) { create :post }
 
   describe 'POST #create' do
-    context 'with valid comment params' do
-      it 'responds successfully with JSON format' do
-        post :create, post_id: new_post.id, comment: {body: 'Test Comment'}, format: :json
+    context 'when user is authenticated' do
+      before { sign_in user }
 
-        expect(response.content_type).to eq('application/json')
+      context 'with valid comment params' do
+        it 'responds successfully with JSON format' do
+          post :create, post_id: new_post.id, comment: {body: 'Test Comment'}, format: :json
+
+          expect(response.content_type).to eq('application/json')
+        end
+
+        it 'creates a new Comment' do
+          post :create, post_id: new_post.id, comment: {body: 'Test Comment'}, format: :json
+
+          expect(Comment.count).to eq(1)
+        end
       end
 
-      it 'creates a new Comment' do
-        post :create, post_id: new_post.id, comment: {body: 'Test Comment'}, format: :json
+      context 'with invalid comment params' do
+        it 'responds successfully with JSON format' do
+          post :create, post_id: new_post.id, comment: {body: nil}, format: :json
 
-        expect(Comment.count).to eq(1)
+          expect(response.content_type).to eq('application/json')
+        end
+
+        it 'does not creates a new Comment' do
+          post :create, post_id: new_post.id, comment: {body: nil}, format: :json
+
+          expect(Comment.count).to eq(0)
+        end
       end
     end
 
-    context 'with invalid comment params' do
-      it 'responds successfully with JSON format' do
-        post :create, post_id: new_post.id, comment: {body: nil}, format: :json
+    context 'when user is not authenticated' do
+      context 'with valid comment params' do
+        it 'does not responds successfully with JSON format' do
+          post :create, post_id: new_post.id, comment: {body: 'Test Comment'}, format: :json
 
-        expect(response.content_type).to eq('application/json')
+          expect(response.content_type).to eq(nil)
+        end
+
+        it 'does not creates a new Comment' do
+          post :create, post_id: new_post.id, comment: {body: 'Test Comment'}, format: :json
+
+          expect(Comment.count).to eq(0)
+          expect(response).to have_http_status(401)
+        end
       end
 
-      it 'does not creates a new Comment' do
-        post :create, post_id: new_post.id, comment: {body: nil}, format: :json
+      context 'with invalid comment params' do
+        it 'does not responds successfully with JSON format' do
+          post :create, post_id: new_post.id, comment: {body: nil}, format: :json
 
-        expect(Comment.count).to eq(0)
+          expect(response.content_type).to eq(nil)
+        end
+
+        it 'does not creates a new Comment' do
+          post :create, post_id: new_post.id, comment: {body: nil}, format: :json
+
+          expect(Comment.count).to eq(0)
+          expect(response).to have_http_status(401)
+        end
       end
     end
   end
@@ -36,29 +73,63 @@ RSpec.describe CommentsController, type: :controller do
   describe 'GET #show' do
     let(:comment) { create :comment, post_id: new_post.id }
 
-    it 'responds successfully with an HTTP 200 status code' do
-      get :show, post_id: new_post.id, id: comment.id, format: :json
+    context 'when user is authenticated' do
+      before { sign_in user }
 
-      expect(response.content_type).to eq('application/json')
-      expect(response).to have_http_status(200)
+      it 'responds successfully with an HTTP 200 status code' do
+        get :show, post_id: new_post.id, id: comment.id, format: :json
+
+        expect(response.content_type).to eq('application/json')
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    context 'when user is not authenticated' do
+      it 'responds successfully with an HTTP 200 status code' do
+        get :show, post_id: new_post.id, id: comment.id, format: :json
+
+        expect(response.content_type).to eq('application/json')
+        expect(response).to have_http_status(200)
+      end
     end
   end
 
   describe 'PUT #upvote' do
     let(:comment) { create :comment, post_id: new_post.id }
 
-    it 'responds successfully with JSON format' do
-      put :upvote, post_id: new_post.id, id: comment.id, format: :json
+    context 'when user is authenticated' do
+      before { sign_in user }
 
-      expect(response.content_type).to eq('application/json')
+      it 'responds successfully with JSON format' do
+        put :upvote, post_id: new_post.id, id: comment.id, format: :json
+
+        expect(response.content_type).to eq('application/json')
+      end
+
+      it 'increases upvotes count' do
+        put :upvote, post_id: new_post.id, id: comment.id, format: :json
+
+        comment.reload
+
+        expect(comment.upvotes).to eq(1)
+      end
     end
 
-    it 'increases upvotes count' do
-      put :upvote, post_id: new_post.id, id: comment.id, format: :json
+    context 'when user is not authenticated' do
+      it 'does not responds successfully with JSON format' do
+        put :upvote, post_id: new_post.id, id: comment.id, format: :json
 
-      comment.reload
+        expect(response.content_type).to eq(nil)
+      end
 
-      expect(comment.upvotes).to eq(1)
+      it 'does not increases upvotes count' do
+        put :upvote, post_id: new_post.id, id: comment.id, format: :json
+
+        comment.reload
+
+        expect(comment.upvotes).to eq(0)
+        expect(response).to have_http_status(401)
+      end
     end
   end
 end
